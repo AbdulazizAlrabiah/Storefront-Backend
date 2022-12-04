c1d81000151312bce5ceb1419b238bc38c3b717c
Abdulaziz m1 <aziz4000@gmail.com>
2022-12-05 02:40:10 +0300

    Add routes

---
+++ src/routes/router.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { getAllUsers, getUser, createUser } from './api/userApis';
+import { getAllProducts, getProduct } from './api/productApis';
+import { getActiveOrder, addProductToOrder } from './api/orderApis';
+import { authenticate } from '../controller/authentication';
+
+const routes = express.Router();
+
+// User
+routes.get('/users', authenticate, getAllUsers);
+routes.get('/user/:id', authenticate, getUser);
+routes.post('/user', createUser);
+
+// Product
+routes.get('/products', getAllProducts);
+routes.get('/product/:id', getProduct);
+
+// Order
+routes.get('/order', authenticate, getActiveOrder);
+routes.post('/order', authenticate, addProductToOrder);
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes/router';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address: string = 'Local Host port 3000';
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use('/api', routes);
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+  console.log(`starting app on: ${address}`);
+});
