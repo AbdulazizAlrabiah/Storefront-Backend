e9f7241ce77d5bb70d117616cd4c467344937b44
Abdulaziz m1 <aziz4000@gmail.com>
2022-12-05 02:38:39 +0300

    Fix database configurations

--- src/database.ts
+++ src/database.ts
@@ -3,31 +3,26 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const env = process.env.Env;
+const env = process.env.ENV;
 
 let client: Pool;
 
-const {
-    HOST,
-    DATABASE,
-    TEST_DATABASE,
-    PASSWORD,
-} = process.env;
+const { HOST, DATABASE, TEST_DATABASE, PASSWORD } = process.env;
 
-if (env == "test") {
-    client = new Pool({
-        host: HOST,
-        database: TEST_DATABASE,
-        password: PASSWORD,
-        port: 5432,
-    });
+if (env == 'test') {
+  client = new Pool({
+    host: HOST,
+    database: TEST_DATABASE,
+    password: PASSWORD,
+    port: 5432,
+  });
 } else {
-    client = new Pool({
-        host: HOST,
-        database: DATABASE,
-        password: PASSWORD,
-        port: 5432,
-    });
+  client = new Pool({
+    host: HOST,
+    database: DATABASE,
+    password: PASSWORD,
+    port: 5432,
+  });
 }
 
 export default client;
