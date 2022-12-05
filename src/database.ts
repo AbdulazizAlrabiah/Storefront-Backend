30a8911716a995b2e2a0d9e0e49b96658a779c9c
Abdulaziz m1 <aziz4000@gmail.com>
2022-12-05 10:05:39 +0300

    Add user for database

--- src/database.ts
+++ src/database.ts
@@ -7,12 +7,13 @@ const env = process.env.ENV;
 
 let client: Pool;
 
-const { HOST, DATABASE, TEST_DATABASE, PASSWORD } = process.env;
+const { HOST, DATABASE, TEST_DATABASE, USER, PASSWORD } = process.env;
 
 if (env == 'test') {
   client = new Pool({
     host: HOST,
     database: TEST_DATABASE,
+    user: USER,
     password: PASSWORD,
     port: 5432,
   });
@@ -20,6 +21,7 @@ if (env == 'test') {
   client = new Pool({
     host: HOST,
     database: DATABASE,
+    user: USER,
     password: PASSWORD,
     port: 5432,
   });
