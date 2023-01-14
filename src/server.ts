e751b2844a79ef1cf32c0d2db2347cb21de6aeb2
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-15 00:00:59 +0300

    Implement cors options

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/router';
+import cors from 'cors';
 
 const app: express.Application = express();
 const address: string = 'Local Host port 3000';
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api', routes);
 
