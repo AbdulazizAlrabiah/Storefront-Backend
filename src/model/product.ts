454bece403324fc539c29af078873a98ed22d9f9
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-15 00:04:12 +0300

    Modify implementation to the new product type

--- src/model/product.ts
+++ src/model/product.ts
@@ -1,10 +1,12 @@
 import client from '../database';
 
 export type Product = {
-  id: number;
-  name: String;
-  price: number;
-  category: String;
+  id: number,
+  name: String,
+  price: number,
+  category: String,
+  imageLink: String,
+  description: String,
 };
 
 export class ProductStore {
@@ -24,6 +26,8 @@ export class ProductStore {
           name: product.name,
           price: parseFloat(product.price),
           category: product.category,
+          imageLink: product.image_link,
+          description: product.description,
         });
       }
 
@@ -36,7 +40,7 @@ export class ProductStore {
   static async showProductAt(id: number): Promise<Product> {
     try {
       const con = await client.connect();
-      const sql = 'SELECT id, name, price, category FROM products WHERE id=$1';
+      const sql = 'SELECT * FROM products WHERE id=$1';
       const result = await con.query(sql, [id]);
 
       con.release();
@@ -48,6 +52,8 @@ export class ProductStore {
         name: result.rows[0].name,
         price: parseFloat(result.rows[0].price),
         category: result.rows[0].category,
+        imageLink: result.rows[0].image_link,
+        description: result.rows[0].description,
       };
 
       return product;
