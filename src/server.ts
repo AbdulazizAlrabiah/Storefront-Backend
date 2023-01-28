ba31181aea43d9e49b08d5fbfaf69c8256695d1c
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-28 22:20:17 +0300

    Assign port conditionally on environment

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import routes from './routes/router';
 import cors from 'cors';
 
 const app: express.Application = express();
-const address: string = 'Local Host port 3000';
+
+const { ENV } = process.env;
 
 const corsOptions = {
   origin: '*',
@@ -15,6 +16,12 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api', routes);
 
+if (ENV === 'test') {
 app.listen(3000, function () {
-  console.log(`starting app on: ${address}`);
+  console.log('starting app on: Localhost:3000');
 });
+} else {
+  app.listen(function () {
+    console.log('starting app');
+  });
+}
