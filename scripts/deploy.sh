f1e5b8b26caefe4a3a033fc617b41f937ca85474
Abdulaziz m1 <aziz4000@gmail.com>
2023-02-02 21:53:14 +0300

    Fix deployment script
    Init eb before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-rm -rf ./node_modules && eb deploy store-front-backend-production
+rm -rf ./node_modules && eb init --region us-east-1 --platform node.js-16 store-front-backend-production && eb deploy store-front-backend-production
