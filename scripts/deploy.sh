a08b5fa80f7b0d3c887de0af40f4a318e5e8d143
Abdulaziz m1 <aziz4000@gmail.com>
2023-02-03 01:01:42 +0300

    Remove the need to delete node modules
    Since we are ignoring it in .ebignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-rm -rf ./node_modules && eb init --region us-east-1 --platform node.js-16 store-front-backend-production && eb deploy store-front-backend-production
+eb init --region us-east-1 --platform node.js-16 store-front-backend-production && eb deploy store-front-backend-production
