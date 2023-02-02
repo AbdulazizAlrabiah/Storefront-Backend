731a8cbf59872cc2a75c3ea6e1a0cbab37aac7f0
Abdulaziz m1 <aziz4000@gmail.com>
2023-02-03 00:46:35 +0300

    Configure connection to prod DB
    + Modify starting prod server

--- src/database.ts
+++ src/database.ts
@@ -5,9 +5,17 @@ dotenv.config();
 
 let client: Pool;
 
-const { ENV, HOST, DATABASE, TEST_DATABASE, USER, PASSWORD } = process.env;
+const { ENV, HOST, DATABASE, TEST_DATABASE, USER, PASSWORD, PROD_DB_HOST, PROD_DB_NAME, PROD_DB_USER, PROD_DB_PASS } = process.env;
 
-if (ENV == 'test') {
+if (ENV == 'prod') {
+  client = new Pool({
+    host: PROD_DB_HOST,
+    database: PROD_DB_NAME,
+    user: PROD_DB_USER,
+    password: PROD_DB_PASS,
+    port: 5432,
+  });
+} else if (ENV == 'test') {
   client = new Pool({
     host: HOST,
     database: TEST_DATABASE,
